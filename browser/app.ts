f27dcfa6da0a2bf22d7070df378196f59ca46470
Philip Grabenhorst <phlip007@gmail.com>
2023-08-22 06:23:35 -0700

    Add styles to fix canvas width

--- browser/app.ts
+++ browser/app.ts
@@ -1,21 +1,11 @@
-import { mountBrowser } from "@/drawing";
+import { Browser } from "@/index";
+let url = "http://browser.engineering/index.html";
+
 
 const browserWrapper = document.getElementById("browser-target") as HTMLDivElement;
 
 function main() {
-    // 1. get the browser and append a canvas
-    const C = document.createElement("canvas");
-    let bb = browserWrapper.getBoundingClientRect();
-    C.width = bb.width;
-    C.height = bb.height;
-    C.style.width = "100%";
-    C.style.height = "100%";
-    browserWrapper.appendChild(C);
-
-    // 2. start the browser with a reference to our new canvas context
-    let ctx = C.getContext("2d");
-    if (!ctx) throw new Error("Cannot mount without a context!");
-    mountBrowser(ctx);
-    
+    let B = new Browser(browserWrapper);
+    B.load(url);
 }
 main();
